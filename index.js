b4874e5fe30f657afe612210adee9766f5df4f5e
Курилин Никита Андреевич <kurilinna@pochtabank.ru>
2024-09-24 19:42:59 +0700

    fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const DB_URL = `mongodb+srv://Nikita:userNikita@cluster0.5808v.mongodb.net/?retr
 const app = express();
 
 app.use(express.json());
-app.use('/api', router);;
+app.use('/api', router);
 
 async function startApp() {
 	try {
@@ -20,24 +20,3 @@ async function startApp() {
 }
 
 startApp();
-
-async function fetchGoogleReviews() {
-	const placeId = 'ChIJD_eix-43UDARlqezo_di3hk';
-	const apiKey = 'AIzaSyCgduk_6in_hms3d5wXYV8PUYk3qv2uP8s';
-	const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=reviews&key=${apiKey}`;
-	// const url1 = `https://mybusiness.googleapis.com/v4/accounts/${apiKey}/locations/${placeId}/reviews`;
-
-
-	try {
-		const response = await fetch(url);
-		const data = await response.json();
-
-		console.log(data.result);
-		return data.result.reviews;
-	} catch (error) {
-		console.error('Failed to fetch reviews:', error);
-		throw error;
-	}
-}
-
-fetchGoogleReviews();
